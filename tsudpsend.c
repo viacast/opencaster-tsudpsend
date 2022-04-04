5d1e7d82fc47f054daf24d65d8db4f17c4a95f3a
Rubens <rubens17rodrigues@gmail.com>
2022-04-04 16:23:15 -0300

    Fix bitrate variavel

--- tsudpsend.c
+++ tsudpsend.c
@@ -62,42 +62,76 @@ long long int usecDiff(struct timespec* time_stop,
   return temp / 1000;
 }
 
-int get_bitrate(int* bitrate_fd, int packet_size, double* bitrate,
-                int* bit_seek) {
-  int i = 0;
-  static int get_last_packet = 0;
+int get_bitrate(int* bitrate_fd, int packet_size_udp, double* bitrate,
+                int* n_packets_udp, int* file_over) {
   int len = 0;
   int has_bitrate = 0;
-  unsigned char ts_packet[TS_PACKET_SIZE]; /* TS packet */
   u_short pid;
   unsigned int pcr_ext = 0;
   unsigned long long int pcr_base = 0;
   unsigned long long int new_pcr = 0;
   unsigned long long int new_pcr_index = 0;
   unsigned char* bitrate_buf;
-  static unsigned long long int ts_packet_count;
+  unsigned long long int ref_packets = 0;
+  static unsigned long long int ts_packet_count = 0;
   static unsigned long long int
       pid_pcr_table[MAX_PID]; /* PCR table for the TS packets */
   static unsigned long long int
       pid_pcr_index_table[MAX_PID]; /* PCR index table for the TS packets */
 
-  bitrate_buf = malloc(packet_size);
+  bitrate_buf = malloc(packet_size_udp);
   /* Read next packet */
 
-  while (1) {
-    if (i % (packet_size / TS_PACKET_SIZE) == 0) {
-      i = 0;
-      len = read(*bitrate_fd, bitrate_buf, packet_size);
-      *bit_seek = lseek(*bitrate_fd, packet_size, SEEK_CUR);
+  fprintf(stderr, "error\n");
 
-      if (len == 0) {
-        // fprintf(stderr, "Final do arquivo\n");
-        // sleep(1);
+  while (1) {
+    /* Get packet UDP */
+    // TODO tratar os ultimos pacotes.
+    int total_lido = 0;
+    if (ts_packet_count % (packet_size_udp / TS_PACKET_SIZE) == 0) {
+      if (has_bitrate) {
+        // fprintf(stderr, "has bitrate\n");
+        free(bitrate_buf);
+
+        lseek(*bitrate_fd, -((*n_packets_udp) * packet_size_udp), SEEK_CUR);
+
         break;
       }
+
+      ref_packets = ts_packet_count;
+      while (total_lido < packet_size_udp) {
+        len = read(*bitrate_fd, bitrate_buf + total_lido,
+                   packet_size_udp - total_lido);
+
+        if (len == 0) {
+          if (total_lido > 0) {
+            *file_over = 1;
+            fprintf(stderr,
+                    "File Over \t N_last packets: %d \t N_last bytes: %d\n\n",
+                    *n_packets_udp, total_lido);
+
+            lseek(*bitrate_fd, -total_lido, SEEK_CUR);
+            lseek(*bitrate_fd, -((*n_packets_udp) * packet_size_udp), SEEK_CUR);
+
+            return 0;
+          }
+          // fprintf(stdout, "Acabou o arquivo \n\n");
+          // fprintf(stdout, "N packets ts : %llu \n\n", ts_packet_count);
+          exit(EXIT_SUCCESS);
+        }
+
+        if (len < 0) {
+          fprintf(stderr, "Error\n");
+          exit(EXIT_FAILURE);
+        }
+
+        if (len > 0) total_lido = total_lido + len;
+      }
+      *n_packets_udp = *n_packets_udp + 1;
     }
 
-    memcpy(ts_packet, (bitrate_buf + (i * TS_PACKET_SIZE)), TS_PACKET_SIZE);
+    unsigned char* ts_packet =
+        (bitrate_buf + ((ts_packet_count - ref_packets) * TS_PACKET_SIZE));
 
     /* check packet */
     memcpy(&pid, ts_packet + 1, 2); /* get the 2nd and 3 bytes*/
@@ -128,60 +162,51 @@ int get_bitrate(int* bitrate_fd, int packet_size, double* bitrate,
         new_pcr = pcr_base * 300 + pcr_ext;
         new_pcr_index = (ts_packet_count * TS_PACKET_SIZE) + 10;
 
-        if (pid_pcr_table[pid] == 0) {
-          pid_pcr_table[pid] = new_pcr;
-          pid_pcr_index_table[pid] = new_pcr_index;
-          continue;
-        }
-
         has_bitrate = 1;
+        *bitrate = (((double)(new_pcr_index - pid_pcr_index_table[pid])) * 8 *
+                    SYSTEM_CLOCK_FREQUENCY) /
+                   ((double)(new_pcr - pid_pcr_table[pid]));
+
+        fprintf(stderr, "Pid : %u\n", pid);
         fprintf(stderr, "new pcr_index : %llu\n", new_pcr_index);
         fprintf(stderr, "old pcr_index : %llu\n", pid_pcr_index_table[pid]);
         fprintf(stderr, "new pcr : %llu\n", new_pcr);
         fprintf(stderr, "old pcr : %llu\n", pid_pcr_table[pid]);
+        fprintf(stderr, "Meu bitrate: %f\n", *bitrate);
 
-        double bitrate_local = 0;
-        bitrate_local = (((double)(new_pcr_index - pid_pcr_index_table[pid])) *
-                         8 * SYSTEM_CLOCK_FREQUENCY) /
-                        ((double)(new_pcr - pid_pcr_table[pid]));
+        fprintf(stderr, "Indice delta is : %llu bytes \n\n\n",
+                new_pcr_index - pid_pcr_index_table[pid]);
 
-        // *bitrate = teste;
-        fprintf(stderr, "Meu bitrate: %f\n", bitrate_local);
-
-        if (bitrate_local >= 1000) {
-          *bitrate = bitrate_local;
-        }
-
-        // *bitrate = (unsigned int)teste;
         pid_pcr_table[pid] = new_pcr;
         pid_pcr_index_table[pid] = new_pcr_index;
       }
-      ts_packet_count++;
-      if (has_bitrate) {
-        fprintf(stderr, "has bitrate\n");
-        i++;
-        *bit_seek = lseek(*bitrate_fd, -(len - (i * TS_PACKET_SIZE)), SEEK_CUR);
-
-        free(bitrate_buf);
-        // sleep(1);
-        break;
-      }
     }
-    i++;
+    ts_packet_count++;
   }
+
+  return 0;
+}
+
+int get_sleep(struct timespec* nanosleep, double bitrate,
+              unsigned long int packet_size) {
+  int one_second_nano = 1000000000;
+  int size_udp_packet_bits = packet_size * 8;
+  double n_packets = (bitrate / size_udp_packet_bits);
+
+  nanosleep->tv_nsec = (one_second_nano / n_packets);
+  fprintf(stderr, "Nano sleep: %lu\n\n", nanosleep->tv_nsec);
   return 0;
 }
 
 int main(int argc, char* argv[]) {
   int sockfd;
+  int n_packets_udp = 0;
   int len;
   int sent;
+  int file_over = 0;
   int ret;
   int is_multicast;
   int transport_fd;
-  int bitrate_fd;
-  int transport_seek;
-  int bit_seek;
   unsigned char option_ttl;
   char start_addr[4];
   struct sockaddr_in addr;
@@ -200,9 +225,9 @@ int main(int argc, char* argv[]) {
   memset(&time_stop, 0, sizeof(time_stop));
   memset(&nano_sleep_packet, 0, sizeof(nano_sleep_packet));
 
-  if (argc < 5) {
+  if (argc < 4) {
     fprintf(stderr,
-            "Usage: %s file.ts ipaddr port bitrate [ts_packet_per_ip_packet] "
+            "Usage: %s file.ts ipaddr port  [ts_packet_per_ip_packet] "
             "[udp_packet_ttl]\n",
             argv[0]);
     fprintf(stderr, "ts_packet_per_ip_packet default is 7\n");
@@ -215,12 +240,12 @@ int main(int argc, char* argv[]) {
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = inet_addr(argv[2]);
   addr.sin_port = htons(atoi(argv[3]));
-  bitrate = atoi(argv[4]);
-  if (bitrate <= 0) {
-    bitrate = 100000000;
-  }
-  if (argc >= 6) {
-    packet_size = strtoul(argv[5], 0, 0) * TS_PACKET_SIZE;
+  // bitrate = atoi(argv[4]);
+  // if (bitrate <= 0) {
+  //   bitrate = 100000000;
+  // }
+  if (argc >= 5) {
+    packet_size = strtoul(argv[4], 0, 0) * TS_PACKET_SIZE;
   } else {
     packet_size = 7 * TS_PACKET_SIZE;
   }
@@ -231,8 +256,8 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  if (argc >= 7) {
-    option_ttl = atoi(argv[6]);
+  if (argc >= 6) {
+    option_ttl = atoi(argv[5]);
     is_multicast = 0;
     memcpy(start_addr, argv[2], 3);
     start_addr[3] = 0;
@@ -258,67 +283,84 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  bitrate_fd = open(tsfile, O_RDONLY);
-  if (bitrate_fd < 0) {
-    fprintf(stderr, "can't open file %s\n", tsfile);
-    close(sockfd);
-    return 0;
-  }
-
   int completed = 0;
   send_buf = malloc(packet_size);
   packet_time = 0;
   real_time = 0;
-
-  nano_sleep_packet.tv_nsec = 665778; /* 1 packet at 100mbps*/
-
-  clock_gettime(CLOCK_MONOTONIC, &time_start);
+  // nano_sleep_packet.tv_nsec = 665778; /* 1 packet at 100mbps*/
 
   while (!completed) {
-    clock_gettime(CLOCK_MONOTONIC, &time_stop);
-    real_time = usecDiff(&time_stop, &time_start);
+    n_packets_udp = 0;
+    get_bitrate(&transport_fd, packet_size, &bitrate, &n_packets_udp,
+                &file_over);
+    get_sleep(&nano_sleep_packet, bitrate, packet_size);
 
-    get_bitrate(&bitrate_fd, packet_size, &bitrate, &bit_seek);
-    while (real_time * bitrate > packet_time * 1000000 &&
-           transport_seek < bit_seek) { /* theorical bits against sent bits */
+    // fprintf(stderr, "N_packets : %d \t bitrate: %f \t\n", n_packets_udp,
+    //         bitrate);
 
-      fprintf(
-          stderr,
-          "Bitrate: %f \t real_time: %llu Teorical bits: %llu \t Sent bits: "
-          "%llu \t Completed: %d\n",
-          bitrate, real_time, (unsigned long long)real_time * packet_time,
-          packet_time * 1000000, completed);
+    clock_gettime(CLOCK_MONOTONIC, &time_start);
 
-      len = read(transport_fd, send_buf, packet_size);
-      transport_seek = lseek(transport_fd, packet_size, SEEK_CUR);
+    packet_time = 0;
+    int i = 0;
+    for (i = 0; i < n_packets_udp; i++) {
+      // fprintf(
+      //     stderr,
+      //     "Bitrate: %f \t real_time: %llu Teorical bits: %.0f \t Sent bits: "
+      //     "%llu \t Completed: %d\n",
+      //     bitrate, real_time, real_time * bitrate / (double)1000000,
+      //     packet_time, completed);
 
-      fprintf(stderr, "Transporte seek : %d \t Bit seek %d\n", transport_seek,
-              bit_seek);
+      // len = read(transport_fd, send_buf, packet_size);
+      int total_lido = 0;
+      while (total_lido < packet_size) {
+        len =
+            read(transport_fd, send_buf + total_lido, packet_size - total_lido);
 
-      if (len < 0) {
-        fprintf(stderr, "ts file read error \n");
-        completed = 1;
-        break;
-      } else if (len == 0) {
-        fprintf(stderr, "ts sent done\n");
-        completed = 1;
-        break;
-      } else {
-        sent = sendto(sockfd, send_buf, len, 0, (struct sockaddr*)&addr,
-                      sizeof(struct sockaddr_in));
-        if (sent <= 0) {
-          perror("send(): error ");
-          completed = 1;
-          break;
-        } else {
-          packet_time += packet_size * 8;
+        if (len <= 0) {
+          fprintf(stdout,
+                  "Acabou o arquivo \t Valor de i: %d \t N_packets: %d \t Len: "
+                  "%d\n\n",
+                  i, n_packets_udp, len);
+          exit(EXIT_SUCCESS);
         }
+
+        if (len > 0) total_lido = total_lido + len;
+      }
+
+      sent = sendto(sockfd, send_buf, len, 0, (struct sockaddr*)&addr,
+                    sizeof(struct sockaddr_in));
+
+      if (sent <= 0) {
+        perror("send(): error ");
+        completed = 1;
+        break;
+      }
+
+      packet_time += packet_size * 8;
+      clock_gettime(CLOCK_MONOTONIC, &time_stop);
+      real_time = usecDiff(&time_stop, &time_start);
+
+      // packet_time * 1000000 is to adjsust with real_time
+      if (real_time * bitrate < packet_time * 1000000) {
+        nanosleep(&nano_sleep_packet, 0);
       }
     }
 
-    nanosleep(&nano_sleep_packet, 0);
-  }
+    if (file_over) {
+      len = read(transport_fd, send_buf, packet_size);
+      sent = sendto(sockfd, send_buf, len, 0, (struct sockaddr*)&addr,
+                    sizeof(struct sockaddr_in));
 
+      if (sent <= 0) {
+        perror("send(): error ");
+        completed = 1;
+        break;
+      }
+
+      fprintf(stderr, "Last bytes");
+      break;
+    }
+  }
   close(transport_fd);
   close(sockfd);
   free(send_buf);
